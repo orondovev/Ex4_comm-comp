f77b4118fcf79d9b0298dd764e41bd975a84fa8e
barakdf <barakdf@gmail.com>
2021-12-21 23:12:32 +0200

    commits

--- Measure.c
+++ Measure.c
@@ -8,22 +8,17 @@
 #include <sys/time.h>
 
 #define TRUE 1
-#define FALSE 0
+
+#define PORT 8080
+
 
 // get the 1mb.txt file
 
-void get_file(int socket) {
-    char buff_file[1024];
-    long mb = 0;
-    int f;
-
-    while (TRUE) {
-        f = recv(socket, buff_file, 1024, 0);
-        if (f <= 0) {
-            break;
-        }
-        mb += f;
-        bzero(buff_file, 1024);
+void use_file(int socket) {
+    char buf[1024];
+    while (recv(socket, buf, 1024, 0) != 0)
+    {
+        bzero(buf, 1024); //clearing the buf
     }
     return;
 }
@@ -32,29 +27,36 @@ void get_file(int socket) {
 int main(int argc, char **argv) {
     char buf[256];
     struct timeval start, end;
-    long long seconds, mil_sec;
-    double reno_time = 0, cubic_time = 0, time_keeper = 0;
+    long seconds, mic_sec;
+    long double reno_time, cubic_time, curr_time = 0;
+
     struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
     int new_sock;
     socklen_t len;
 
 
+    //open new socket.
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
         perror("socket");
         return -1;
     }
 
-    len = sizeof(buf);
-    if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) {
-        perror("getsockopt");
-        return -1;
+
+    len = strlen(buf);
+    int yes = 1;
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1) {
+        perror("setsockopt");
+        exit(1);
     }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
 
 
+
+    //bind socket to port
     if (bind(sock, (struct sockaddr *) &address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
@@ -66,49 +68,76 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
+    printf("Waiting for Connections..\n");
+
     unsigned long addr_len = sizeof(address);
+
+    //ready to receive first 5 packets.
     for (int i = 0; i < 5; i++) {
+        memset(&address, 0, sizeof(address));
         if ((new_sock = accept(sock, (struct sockaddr *) &address, (socklen_t *) &addr_len) < 0)) {
-            perror("accept");
+            perror("got error from accept");
             exit(EXIT_FAILURE);
         }
 
         len = sizeof(buf);
         if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) {
-            perror("getsockopt");
+            perror("got error from getsockopt");
             return -1;
         }
-        printf("Current cc is: %s\n", buf);
+        printf("Current cc: %s\n", buf);//todo change
 
         gettimeofday(&start, 0);
-        get_file(new_sock);
+        use_file(new_sock);
         gettimeofday(&end, 0);
         seconds = end.tv_sec - start.tv_sec;
-        mil_sec = end.tv_usec - start.tv_usec;
-        time_keeper += (seconds + mil_sec * 1e-6);// most accurate
+        mic_sec = end.tv_usec - start.tv_usec;
+        curr_time += (double) (seconds + mic_sec * 1e-6);// most accurate
 
     }
+    cubic_time = (curr_time / 5);
 
-    cubic_time = (time_keeper / 5);
-
-    time_keeper = 0;
-
-    printf("Current: %s\n", buf);
+    curr_time = 0;
 
     strcpy(buf, "reno");
     len = strlen(buf);
     if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0) {
-        perror("setsockopt");
+        perror("error in setsockopt");
         return -1;
     }
     len = sizeof(buf);
-    if (
-            getsockopt(sock, IPPROTO_TCP,
-                       TCP_CONGESTION, buf, &len) != 0) {
+    if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) {
         perror("getsockopt");
         return -1;
     }
-    printf("New: %s\n", buf);
+    printf("New cc: %s\n", buf);
+
+    //ready to receive second 5 packets.
+    for (int i = 0; i < 5; i++) {
+        memset(&address, 0, sizeof(address));
+        if ((new_sock = accept(sock, (struct sockaddr *) &address, (socklen_t *) &addr_len) < 0)) {
+            perror("got error from accept");
+            exit(EXIT_FAILURE);
+        }
+
+        len = sizeof(buf);
+        if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) {
+            perror("got error from getsockopt");
+            return -1;
+        }
+        printf("Current cc: %s\n", buf);
+
+        gettimeofday(&start, 0);
+        use_file(new_sock);
+        gettimeofday(&end, 0);
+        seconds = end.tv_sec - start.tv_sec;
+        mic_sec = end.tv_usec - start.tv_usec;
+        curr_time += (double) (seconds + mic_sec * 1e-6);// most accurate
+
+    }
+    reno_time = (curr_time / 5);
+    printf("cubic average time: %Lf\n", cubic_time);
+    printf("reno average time: %Lf\n", reno_time);
     close(sock);
     return 0;
 }
--- sender.c
+++ sender.c
@@ -1,14 +1,107 @@
-#include "sender.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
 
-// open socket tcp
+#define PORT 8080 //Port 8080 is typically used for a personally hosted web server.
 
-//connect with measure
+int main(int argc, char **argv) {
 
-//send file 5 times
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT); //cast as "host to network".
 
-//change algorithm CC
+    char file_buf[1025] = "";
 
-//send again the file 5 times
+    char buf[256];
+    socklen_t len;
+    int sock = 0;
 
-//close connection
+    FILE *fp;
+    char *f_name = "1mb.txt";
+
+
+    //init a new socket and open the connection with server -->
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("socket error\n");
+        return -1;
+    }
+    printf("socket has created.\n");
+
+    //converting ip address to binary.
+    int convert_ip_status = inet_pton(AF_INET, "10.0.0.27", &server_address.sin_addr);
+    if (convert_ip_status <= 0) {
+        printf("Invalid address \n");
+        return -1;
+    }
+
+
+    //open and read from given file.
+    fp = fopen(f_name, "r");
+    if (fp == NULL) {
+        perror("error reading the given file");
+        exit(1);
+    }
+
+    //connecting to server.
+    int con_status = connect(sock, (struct sockaddr *) &server_address, sizeof(server_address));
+    if (con_status < 0) {
+        perror("Connection Failed\n");
+        return -1;
+    }
+    printf("successfully connected to measure\n");
+
+    len = sizeof(buf);
+    if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) {
+        perror("getsockopt\n");
+        return -1;
+    }
+
+    printf("Current cc: %s\n", buf);
+    //start the first deliveries.
+    for (int i = 0; i < 10; ++i) {
+
+        // the 5 last packets will send in "reno" cc;
+        if (i >= 5) {
+            strcpy(buf, "reno");
+            len = strlen(buf);
+            if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0) {
+                perror("setsockopt");
+                return -1;
+            }
+            len = sizeof(buf);
+            if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) {
+                perror("getsockopt");
+                return -1;
+            }
+            printf("New cc: %s\n", buf);
+        }
+
+
+
+        //send the file to server (measure).
+        while (fgets(file_buf, 1025, fp) != NULL) {
+            long send_status = send(sock, file_buf, sizeof(file_buf), 0);
+            if (send_status == -1) {
+                perror("Error reading the file data\n");
+                exit(1);
+            }
+
+            //clear the file buffer.
+            bzero(file_buf, 1025);
+        }
+
+        printf("finished to send file\n");
+    }
+    close(sock);
+
+
+    return 0;
+}
 
